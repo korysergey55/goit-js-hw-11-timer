eb3cf3f1a7617989269e7102f59fc94df3251331
Serhiy Korytskyy <korysergey55@gmail.com>
2021-05-15 23:04:07 +0300

    tt

--- js/my-practise-date.js
+++ js/my-practise-date.js
@@ -1,32 +1,34 @@
-const stopTimer = 5;
-let counterTimer = null;
+// const stopTimer = 5;
+// let counterTimer = null;
 
-const timer = {
-  start() {
-    const startTime = Date.now();
-    setInterval(() => {
-      if (counterTimer === stopTimer) return;
-      const currentTime = Date.now();
-      counterTimer += 1;
-      const deltaTime = currentTime - startTime;
-      const timeComponents = getTimeComponents(deltaTime)
-    //   console.log("current date", currentTime);
-    //   console.log("difference", startTime - currentTime);
-      console.log(timeComponents);
-    }, 1000);
-  },
-};
-timer.start(); 
+// const timer = {
+//   start() {
+//     const startTime = Date.now();
+//     setInterval(() => {
+//       if (counterTimer === stopTimer) return;
+//       const currentTime = Date.now();
+//       counterTimer += 1;
+//       const deltaTime = currentTime - startTime;
+//       const timeComponents = getTimeComponents(deltaTime);
+//       //   console.log("current date", currentTime);
+//       //   console.log("difference", startTime - currentTime);
+//       console.log(timeComponents);
+//     }, 1000);
+//   },
+// };
+// timer.start();
 
-// Принимает число, приводит к строке и длбавляет 0  если число меньше 2-х знаков
-function pad(value){
-    return String(value).padStart(2, '0');
-}
+// // Принимает число, приводит к строке и длбавляет 0  если число меньше 2-х знаков
+// function pad(value) {
+//   return String(value).padStart(2, "0");
+// }
 
-function getTimeComponents(time) {
-  const days =  pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-  const mins =  pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs =  pad(Math.floor((time % (1000 * 60)) / 1000));
-  return { days, hours, mins, secs };
-}
+// function getTimeComponents(time) {
+//   const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+//   const hours = pad(
+//     Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+//   );
+//   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+//   const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+//   return { days, hours, mins, secs };
+// }
