4874b11d75aafe672dba33dc7c5f80a362f4ff4b
Serhiy Korytskyy <korysergey55@gmail.com>
2021-05-15 17:10:11 +0300

    my-practice

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// Плагин ожидает следующую HTML-разметку и показывает четыре цифры: дни, часы, минуты и секунды в формате XX:XX:XX:XX.
+//  Количество дней может состоять из более чем двух цифр.
+// Плагин это класс CountdownTimer, экземпляр которого создает новый таймер с настройками.
+
+// new CountdownTimer({
+//   selector: '#timer-1',
+//   targetDate: new Date('Jul 17, 2019'),
+// });
+
+const CountdownTimer = document.querySelector("#timer-1");
+const data = new Date("Jul 17, 2019");
+console.log(data);
+
