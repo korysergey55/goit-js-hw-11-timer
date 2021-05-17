750c1ce2f78577a1e08753929e1e5bf914bd15bc
Serhiy Korytskyy <korysergey55@gmail.com>
2021-05-17 17:42:26 +0300

    hw-done

--- js/index.js
+++ js/index.js
@@ -16,19 +16,19 @@ class CountdownTimer {
       daysElem: document.querySelector(
         `${this.selector} span[data-value='days']`
       ),
-      hoursRef: document.querySelector(
+      hoursElem: document.querySelector(
         `${this.selector} span[data-value='hours']`
       ),
-      minsRef: document.querySelector(
+      minsElem: document.querySelector(
         `${this.selector} span[data-value='mins']`
       ),
-      secsRef: document.querySelector(
+      secsElem: document.querySelector(
         `${this.selector} span[data-value='secs']`
       ),
     };
   }
   changeDate() {
-    const time = this.targetDate.getTime() - new.Date().getTime();
+    const time = this.targetDate.getTime() - new Date().getTime();
     if(time > 0){
     this.getTimeComponents(time);
     } else{
@@ -60,44 +60,8 @@ class CountdownTimer {
 }
 const timerClass = new CountdownTimer({
   selector: "#timer-1",
-  targetDate: new Date("Jul 21, 2021"),
+  targetDate: new Date("Jun 1, 2021"),
 });
 
 timerClass.runTimer();
 
-// const dataInPast = new Date("Jul 17, 2019");
-// function printdataInPast(data) {
-//   const padDate = (num) => String(num).padStart(2, 0);
-//   const yearInPast = dataInPast.getFullYear();
-//   const monthInPast = padDate(dataInPast.getMonth() + 1);
-//   const dayInPast = padDate(dataInPast.getDate());
-//   const print_data = `${yearInPast}-${monthInPast}-${dayInPast}`;
-
-//   return print_data;
-// }
-// printdataInPast(dataInPast);
-// console.log(printdataInPast(dataInPast));
-
-// const timerRef = document.querySelector("#timer-1");
-// const daysRef = document.querySelector("[data-value='days']");
-// const hoursRef = document.querySelector("[data-value='hours']");
-// const minsRef = document.querySelector("[data-value='mins']");
-// const secsRef = document.querySelector("[data-value='secs']");
-
-// const stopTimer = 100;
-// let counterTimer = null;
-
-// const timer = {
-//   start() {
-//     const startTime = Date.now();
-//     setInterval(() => {
-//       if (counterTimer === stopTimer) return;
-//       counterTimer += 1;
-//       const currentTime = Date.now();
-//       const deltaTime = currentTime - startTime;
-//       const timeComponents = getTimeComponents(deltaTime);
-//       console.log(timeComponents);
-//     }, 1000);
-//   },
-// };
-// timer.start();
